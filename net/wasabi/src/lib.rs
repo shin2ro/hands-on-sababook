f5b345ef9f08fe102d16d6f86ee3979948516e17
Shinjiro Sakaitani <ssakaitani@gmail.com>
2024-11-16 22:54:49 +0900

    Add HTTP client

--- net/wasabi/src/lib.rs
+++ net/wasabi/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
+extern crate alloc;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod http;
